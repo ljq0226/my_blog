e0cfbb2f01235e539e4e9afffbef48fc36553e26
ljq0226 <1035821043@qq.com>
2022-10-11 21:34:07 +0800

    测试2

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'luokui'
+const test = () => {
+  return (
+    <div>test
+      {/* <Button className='btn-circle'>asd</Button> */}
+      <div className='underline'>123</div>
+
+    </div>
+  )
+}
+
+export default test
